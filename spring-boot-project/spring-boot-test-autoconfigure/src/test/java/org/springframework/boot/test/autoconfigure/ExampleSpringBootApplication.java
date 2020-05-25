31f2bdab8359293c124024f4a77458e50e8f3bb4
wb-xh666609 <wb-xh666609@alibaba-inc.com>
2020-05-25 20:28:28 +0800

    springboot启动主流程

--- spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/ExampleSpringBootApplication.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/ExampleSpringBootApplication.java
@@ -16,6 +16,7 @@
 
 package org.springframework.boot.test.autoconfigure;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,5 +30,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootConfiguration
 @EnableAutoConfiguration
 public class ExampleSpringBootApplication {
+
+	public static void main(String[] args) {
+		/** 启动流程
+		 * 1. new SpringApplication(Class<?>[] primarySources)
+		 * 	  赋值resourceLoader primarySources webApplicationType
+		 * 	  mainApplicationClass initializers  listeners
+		 * 2. 设置java.awt.headless属性
+		 * 3. prepareEnvironment方法，初始化environment
+		 * 4. printBanner 打印banner
+		 * 5. createApplicationContext 创建context
+		 * 6. prepareContext 做一些准备工作
+		 * 7. refreseContext 启动容器 spring-framework的范畴 很多功能依赖后置处理器
+		 * 8. afterContext 留给子类扩展
+		 * 9. 回调ApplicationRunner 和 CommandLineRunner
+		 * 10. 中间穿插SpringApplicationRunListener的各种生命周期回调函数
+		 */
+		SpringApplication.run(ExampleSpringBootApplication.class, args);
+	}
 
 }
